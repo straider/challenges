90d5560c3a912882c6ca54398d1e3d3cb69e4fe0
José Carlos Monteiro <straider@sapo.pt>
2016-09-28 17:44:55 +0100

    Added minimal Git clone functionality, using JGit.

--- GroovierBitbucketClient/bin/gbc.groovy
+++ GroovierBitbucketClient/bin/gbc.groovy
@@ -3,9 +3,15 @@
 import groovy.json.JsonSlurper
 import javax.xml.bind.DatatypeConverter
 
-String scriptFolder = new File( getClass().protectionDomain.codeSource.location.path ).parent
+String scriptFolder = new File( this.class.protectionDomain.codeSource.location.path ).parent
 String rootFolder   = new File( scriptFolder ).parent
-println "Script Folder: ${scriptFolder}"
+println "Root Folder: ${rootFolder}"
+
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.api.errors.GitAPIException
+import org.eclipse.jgit.api.errors.InvalidRemoteException
+import org.eclipse.jgit.api.errors.TransportException
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 
 File   configurationFile   = new File( "${rootFolder}/etc/configuration.json" )
 String configurationFolder = configurationFile.parent
@@ -38,25 +44,43 @@ connection = address.toURL().openConnection()
 connection.setRequestProperty( "Authorization", basicAuthentication )
 responseStream = connection.getInputStream()
 
+// Change to Map to count cloned / pulled repositories and errored operations
 List repositories = []
 
 response = slurper.parseText( responseStream.text )
 response.values.each {
     List   links
     String reference
-    
+
     links     = it.links.self.href
     reference = links.find() { it.startsWith( 'http' ) }
     // println "Browse Repository: ${reference}"
-    
+
     links     = it.links.clone.href
     reference = links.find() { it.startsWith( 'http' ) }
     repositories << reference
     // println "Clone Repository: ${reference}"
-    
+
     File   repository     = new File( reference )
     String repositoryName = repository.name.replace( '.git', '' )
     println "Repository Name: ${repositoryName}"
+
+    File workingFolder = new File( "${folder}/${repositoryName}" )
+
+    if( workingFolder.isDirectory() ) {
+    } else {
+        workingFolder.mkdirs()
+
+        Git clone = Git.cloneRepository()
+                        .setURI( reference )
+                        .setDirectory( workingFolder )
+                        .setBare( false )
+                        .setCloneAllBranches( true )
+                        .setCredentialsProvider( new UsernamePasswordCredentialsProvider( username, password ) )
+                        .call()
+        println "Cloned repository ${repositoryName}"
+        clone.close()
+    }
 }
 
 println "Found ${repositories.size()} repositories"
