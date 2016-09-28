53172b57009aebbae824c4064bb38fdae244a8ca
José Carlos Monteiro <straider@sapo.pt>
2016-09-28 12:57:29 +0100

    Refactored, with minor code beautifications.

--- GroovierBitbucketClient/bin/gbc.groovy
+++ GroovierBitbucketClient/bin/gbc.groovy
@@ -3,14 +3,15 @@
 import groovy.json.JsonSlurper
 import javax.xml.bind.DatatypeConverter
 
-scriptFolder        = new File( getClass().protectionDomain.codeSource.location.path ).parent
-rootFolder          = new File( scriptFolder ).parent
-configurationFile   = new File( "${rootFolder}/etc/configuration.json" )
-configurationFolder = configurationFile.parent
-
+String scriptFolder = new File( getClass().protectionDomain.codeSource.location.path ).parent
+String rootFolder   = new File( scriptFolder ).parent
 println "Script Folder: ${scriptFolder}"
+
+File   configurationFile   = new File( "${rootFolder}/etc/configuration.json" )
+String configurationFolder = configurationFile.parent
+
 println "Configuration Folder: ${configurationFolder}"
-println "Configuration File: ${configurationFile}"
+println "Configuration File: ${configurationFile.name}"
 
 JsonSlurper slurper = new JsonSlurper()
 
@@ -22,25 +23,40 @@ String project  = configuration.project
 String username = configuration.username
 String password = configuration.password
 String folder   = configuration.folder
-String address  = "http://${hostname}:${port}/rest/api/1.0/projects/${project}/repos?limit=100"
-println "URL: ${address}"
-
-String basicAuthentication = "Basic " + DatatypeConverter.printBase64Binary( "${username}:${password}".getBytes() );
-
-URLConnection connection = address.toURL().openConnection();
-connection.setRequestProperty( "Authorization", basicAuthentication );
-InputStream responseStream = connection.getInputStream();
-
-List references = []
-
-Map response = slurper.parseText( responseStream.text )
-response.values.each {
-    List links = it.links.clone.href
-    String reference = links.find() { it.startsWith( 'http' ) }
-    references << reference
-}
-
 println "Working Folder: ${folder}"
-references.each {
-    println "${it}"
+
+String address  = "http://${hostname}:${port}/rest/api/1.0/projects/${project}/repos?limit=100"
+println "Project URL: ${address}"
+
+String basicAuthentication = "Basic " + DatatypeConverter.printBase64Binary( "${username}:${password}".getBytes() )
+
+URLConnection connection
+InputStream responseStream
+Map response
+
+connection = address.toURL().openConnection()
+connection.setRequestProperty( "Authorization", basicAuthentication )
+responseStream = connection.getInputStream()
+
+List repositories = []
+
+response = slurper.parseText( responseStream.text )
+response.values.each {
+    List   links
+    String reference
+    
+    links     = it.links.self.href
+    reference = links.find() { it.startsWith( 'http' ) }
+    // println "Browse Repository: ${reference}"
+    
+    links     = it.links.clone.href
+    reference = links.find() { it.startsWith( 'http' ) }
+    repositories << reference
+    // println "Clone Repository: ${reference}"
+    
+    File   repository     = new File( reference )
+    String repositoryName = repository.name.replace( '.git', '' )
+    println "Repository Name: ${repositoryName}"
 }
+
+println "Found ${repositories.size()} repositories"
