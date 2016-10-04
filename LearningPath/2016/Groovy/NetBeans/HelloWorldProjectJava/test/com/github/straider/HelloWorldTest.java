ef57e530a2c7731d181207f264d7c7f83b120819
José Carlos Monteiro <straider@sapo.pt>
2016-10-05 00:35:43 +0100

    Cleaned up unnecessary comments in HelloWorld using Java Project in NetBeans.

--- LearningPath/2016/Groovy/NetBeans/HelloWorldProjectJava/test/com/github/straider/HelloWorldTest.java
+++ LearningPath/2016/Groovy/NetBeans/HelloWorldProjectJava/test/com/github/straider/HelloWorldTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.straider;
 
 import org.junit.After;
@@ -11,10 +6,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- *
- * @author jocamo
- */
+import static org.junit.Assert.fail;
+
 public class HelloWorldTest {
 
     public HelloWorldTest() {
@@ -36,15 +29,11 @@ public class HelloWorldTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of main method, of class HelloWorld.
-     */
     @Test
     public void testMain() {
         System.out.println( "main" );
         String[] args = null;
         HelloWorld.main( args );
-        // TODO review the generated test code and remove the default call to fail.
         fail( "The test case is a prototype." );
     }
 
