a24ae659220d5f0b773fbffaf84d434853b113d7
José Carlos Monteiro <straider@sapo.pt>
2016-10-17 21:59:33 +0100

    Added production and test classes to Java Project.

---
+++ LearningPath/2016/Java/PigLatin/UsingJavaProject/src/test/java/com/github/straider/PigLatinerTest.java
@@ -0,0 +1,16 @@
+package com.github.straider;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class PigLatinerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
