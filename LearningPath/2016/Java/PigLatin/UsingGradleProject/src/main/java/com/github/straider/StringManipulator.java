4732b64b4e63061ae6a5251b94009c6d89650d0a
José Carlos Monteiro <straider@sapo.pt>
2016-10-17 23:20:12 +0100

    GREEN: convertToTitlecase()

--- LearningPath/2016/Java/PigLatin/UsingGradleProject/src/main/java/com/github/straider/StringManipulator.java
+++ LearningPath/2016/Java/PigLatin/UsingGradleProject/src/main/java/com/github/straider/StringManipulator.java
@@ -1,4 +1,15 @@
 package com.github.straider;
 
 public class StringManipulator {
+
+    private final String string;
+
+    public StringManipulator( String string ) {
+        this.string = string;
+    }
+
+    public String toTitlecase() {
+        return string.substring( 0, 1 ).toUpperCase() + string.substring( 1 );
+    }
+
 }
