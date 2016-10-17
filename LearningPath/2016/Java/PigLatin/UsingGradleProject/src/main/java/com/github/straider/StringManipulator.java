bdfee1eace045466f72dad4f11e4aea4326e15a6
José Carlos Monteiro <straider@sapo.pt>
2016-10-17 23:36:54 +0100

    GREEN: getIndexOfFirstVowel()

--- LearningPath/2016/Java/PigLatin/UsingGradleProject/src/main/java/com/github/straider/StringManipulator.java
+++ LearningPath/2016/Java/PigLatin/UsingGradleProject/src/main/java/com/github/straider/StringManipulator.java
@@ -2,6 +2,8 @@ package com.github.straider;
 
 public class StringManipulator {
 
+    private static final String VOWELS = "aeiou";
+
     private final String string;
 
     public StringManipulator( String string ) {
@@ -12,4 +14,16 @@ public class StringManipulator {
         return string.substring( 0, 1 ).toUpperCase() + string.substring( 1 ).toLowerCase();
     }
 
+    public int getIndexOfFirstVowel() {
+        int index = 0;
+
+        for ( int at = 0; at < string.length(); at++ ) {
+            if ( VOWELS.contains( Character.toString( string.charAt( at ) ).toLowerCase() ) ) {
+                index = at;
+                break;
+            }
+        }
+
+        return index;
+    }
 }
