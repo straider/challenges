e947f232b63fe073e270da92ebbc16f855bba4a0
José Carlos Monteiro <straider@sapo.pt>
2016-10-18 18:56:22 +0100

    GREEN: translationOfLowercaseWordStartingWithConsonant()

--- LearningPath/2016/Java/PigLatin/UsingMavenArchetypeProject/src/main/java/com/github/straider/PigLatiner.java
+++ LearningPath/2016/Java/PigLatin/UsingMavenArchetypeProject/src/main/java/com/github/straider/PigLatiner.java
@@ -2,8 +2,22 @@ package com.github.straider;
 
 public class PigLatiner {
 
+    private static final String VOWELS = "aeiou";
+
     public String translate( String word ) {
-        return String.format( "%s%s", word, "way" );
+        final String reword;
+        final String suffix;
+
+        final Character firstLetter = word.charAt( 0 );
+        if ( VOWELS.contains( firstLetter.toString() ) ) {
+            suffix = "way";
+            reword = word;
+        } else {
+            suffix = "ay";
+            reword = word.substring( 1 ) + firstLetter;
+        }
+
+        return String.format( "%s%s", reword, suffix );
     }
 
 }
