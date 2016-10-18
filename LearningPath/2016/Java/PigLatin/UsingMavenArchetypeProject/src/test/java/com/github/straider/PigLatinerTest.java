6224722e85f36c5d6bc2bd85a1214f3af86fe306
José Carlos Monteiro <straider@sapo.pt>
2016-10-18 18:33:51 +0100

    Added production and test classes to Maven Archetype Project.

---
+++ LearningPath/2016/Java/PigLatin/UsingMavenArchetypeProject/src/test/java/com/github/straider/PigLatinerTest.java
@@ -0,0 +1,15 @@
+package com.github.straider;
+
+import junit.framework.TestCase;
+
+public class PigLatinerTest extends TestCase {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+}
