321be5c1c17e17f0ff86f0812166bf2d811209ca
José Carlos Monteiro <straider@sapo.pt>
2016-11-18 18:13:54 +0000

    Added CamelContext to SpringApplicationContext.

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelHelloWorld/src/main/java/com/github/straider/camel/MainSpring.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelHelloWorld/src/main/java/com/github/straider/camel/MainSpring.java
@@ -1,5 +1,7 @@
 package com.github.straider.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +9,13 @@ public class MainSpring {
 
     public static void main( final String[] arguments ) throws Exception {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext( "springRoute.xml" );
+
+        CamelContext camelContext = SpringCamelContext.springCamelContext( applicationContext, false );
+        try {
+            camelContext.start();
+        } finally {
+            camelContext.stop();
+        }
     }
 
 }
