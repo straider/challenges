bff757b006d49331d452642e2b405ca2e424e92a
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 00:33:40 +0000

    Added XML and CSV files to be handled by SpringContext.

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelIntegrationNirvana/src/main/java/com/github/straider/camel/SpringContext.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelIntegrationNirvana/src/main/java/com/github/straider/camel/SpringContext.java
@@ -1,9 +1,12 @@
 package com.github.straider.camel;
 
+import org.apache.activemq.util.IOHelper;
 import org.apache.camel.CamelContext;
 import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.File;
 
 public class SpringContext {
 
@@ -12,7 +15,20 @@ public class SpringContext {
 
         CamelContext camelContext = SpringCamelContext.springCamelContext( applicationContext, false );
         try {
+            final File folder  = new File( "target/placeorder" );
+            final File xmlFile = new File( "target/classes/data/message.xml" );
+            final File csvFile = new File( "target/classes/data/message.csv" );
+
+            IOHelper.deleteChildren( folder );
+            IOHelper.deleteFile( folder );
+            IOHelper.mkdirs( folder );
+
+            IOHelper.copyFile( xmlFile, folder );
+            IOHelper.copyFile( csvFile, folder );
+
             camelContext.start();
+
+            Thread.sleep( 2500 );
         } finally {
             camelContext.stop();
         }
