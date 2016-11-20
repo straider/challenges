f8f6cc929cf582e7565117ce4e44acca0d80f859
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 21:35:14 +0000

    Added CustomerServiceBeanBasedRoute and its SpringCamelContext.

---
+++ LearningPath/2016/EnterpriseIntegration/Camel/REST/src/main/java/com/github/straider/camel/CustomerServiceBeanBasedRoute.java
@@ -0,0 +1,17 @@
+package com.github.straider.camel;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+
+public class CustomerServiceBeanBasedRoute extends RouteBuilder {
+
+    private static final String INBOUND_ROUTE = "cxfrs://bean:rsServer";
+
+    @Override
+    public void configure() throws Exception {
+        from( INBOUND_ROUTE )
+                .log( LoggingLevel.INFO, "Received request with body: ${body}" )
+        ;
+    }
+
+}
