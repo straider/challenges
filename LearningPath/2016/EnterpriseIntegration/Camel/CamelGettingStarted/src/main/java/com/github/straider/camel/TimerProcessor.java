eed981ed3ac238e15600fc96ec86bf6916c3d9a0
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 18:54:11 +0000

    Added TimerProcessor.

---
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerProcessor.java
@@ -0,0 +1,17 @@
+package com.github.straider.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TimerProcessor implements Processor {
+
+    private static final Logger logger = LoggerFactory.getLogger( TimerRouteBuilder.class );
+
+    @Override
+    public void process(final Exchange exchange ) throws Exception {
+        logger.info( "Processing {}", exchange );
+    }
+
+}
