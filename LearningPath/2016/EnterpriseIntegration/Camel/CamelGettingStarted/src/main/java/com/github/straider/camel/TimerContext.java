92004bdb2928a47f0ad63b17f3c79e062cd61c95
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 18:59:43 +0000

    Built the Camel context, without blocking until CTRL+C.

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerContext.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerContext.java
@@ -1,6 +1,7 @@
 package com.github.straider.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +10,20 @@ public class TimerContext {
 
     private static final Logger logger = LoggerFactory.getLogger( TimerContext.class );
 
-    public static void main( final String[] arguments ) {
+    public static void main( final String[] arguments ) throws Exception {
         new TimerContext().run();
     }
 
-    private void run() {
+    private void run() throws Exception {
         final CamelContext camelContext = new DefaultCamelContext();
+
+        camelContext.addRoutes( createRouteBuilder() );
+        camelContext.setTracing( true );
+        camelContext.start();
+    }
+
+    private RoutesBuilder createRouteBuilder() {
+        return new TimerRouteBuilder();
     }
 
 }
