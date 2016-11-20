9020fa02f285a42616bc5c9ee35cf89fb0482a29
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 00:31:41 +0000

    Improved SpringContext with CamelContext start() and stop().

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelIntegrationNirvana/src/main/java/com/github/straider/camel/SpringContext.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelIntegrationNirvana/src/main/java/com/github/straider/camel/SpringContext.java
@@ -1,5 +1,7 @@
 package com.github.straider.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,6 +9,13 @@ public class SpringContext {
 
     public static void main(String[] args) throws Exception {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext( "spring-context.xml" );
+
+        CamelContext camelContext = SpringCamelContext.springCamelContext( applicationContext, false );
+        try {
+            camelContext.start();
+        } finally {
+            camelContext.stop();
+        }
     }
 
 }
