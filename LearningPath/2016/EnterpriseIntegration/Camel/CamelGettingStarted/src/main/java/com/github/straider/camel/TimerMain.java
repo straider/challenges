502105b083595aa07320a83d57cd483cda4ec137
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 19:10:00 +0000

    Added route to main Camel context.

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerMain.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerMain.java
@@ -1,5 +1,6 @@
 package com.github.straider.camel;
 
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.main.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,8 +9,20 @@ public class TimerMain extends Main {
 
     private static final Logger logger = LoggerFactory.getLogger( TimerMain.class );
 
-    public static void main( final String[] arguments ) {
+    public static void main( final String[] arguments ) throws Exception {
         Main main = new TimerMain();
+
+        main.enableHangupSupport();
+        main.addRouteBuilder( createRouteBuilder() );
+        if ( arguments.length > 0 ) {
+            main.run( arguments );
+        } else {
+            main.run();
+        }
+    }
+
+    private static RouteBuilder createRouteBuilder() {
+        return new TimerRouteBuilder();
     }
 
 }
