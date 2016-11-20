0c2640502476ed91f89a1f7e6d21bfad7cd191f7
José Carlos Monteiro <straider@sapo.pt>
2016-11-20 19:56:30 +0000

    Added route with beans to main Camel context.

--- LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerBeansMain.java
+++ LearningPath/2016/EnterpriseIntegration/Camel/CamelGettingStarted/src/main/java/com/github/straider/camel/TimerBeansMain.java
@@ -1,15 +1,22 @@
 package com.github.straider.camel;
 
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.main.Main;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TimerBeansMain extends Main {
 
-    private static final Logger logger = LoggerFactory.getLogger( TimerMain.class );
-
     public static void main( final String[] arguments ) throws Exception {
         Main main = new TimerBeansMain();
+
+        main.enableHangupSupport();
+        main.bind( "processByBeanA", new TimerBeanProcessor( "1st" ) );
+        main.bind( "processByBeanB", new TimerBeanProcessor( "2nd" ) );
+        main.addRouteBuilder( createRouteBuilder() );
+        main.run( arguments );
+    }
+
+    private static RouteBuilder createRouteBuilder() {
+        return new TimerBeansRouteBuilder();
     }
 
 }
