7f50df729cb619c9227e34a6c1c7c477b0f18d1e
José Carlos Monteiro <straider@sapo.pt>
2016-11-23 21:45:29 +0000

    Added PigLatinTranslator and Spring Beans and OSGi Context XML files.

---
+++ LearningPath/2016/Java/OSGi/Examples/SprintDynamicModulesTranslator/producer/src/main/java/com/github/straider/osgi.producer/PigLatinTranslator.java
@@ -0,0 +1,12 @@
+package com.github.straider.osgi.producer;
+
+import com.github.straider.osgi.common.Translator;
+
+public class PigLatinTranslator implements Translator {
+
+    @Override
+    public String translate(String text) {
+        return null;
+    }
+
+}
