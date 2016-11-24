e1778f6c4218b1c43bbab22d3f8c72b52abf1b7d
José Carlos Monteiro <straider@sapo.pt>
2016-11-24 21:34:47 +0000

    Replaced sample producer code.

--- LearningPath/2016/Java/OSGi/Examples/PaxSpringMailer/producer/src/main/java/com/github/straider/osgi/producer/internal/MailerActivator.java
+++ LearningPath/2016/Java/OSGi/Examples/PaxSpringMailer/producer/src/main/java/com/github/straider/osgi/producer/internal/MailerActivator.java
@@ -6,40 +6,32 @@ import java.util.Properties;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-import com.github.straider.osgi.producer.ExampleService;
+import com.github.straider.osgi.producer.IMailerService;
 
-/**
- * Extension of the default OSGi bundle activator
- */
-public final class ExampleActivator
-    implements BundleActivator
-{
+public final class MailerActivator implements BundleActivator {
+
     /**
      * Called whenever the OSGi framework starts our bundle
      */
-    public void start( BundleContext bc )
-        throws Exception
-    {
+    public void start( BundleContext bc ) throws Exception {
         System.out.println( "STARTING com.github.straider.osgi.producer" );
 
         Dictionary props = new Properties();
         // add specific service properties here...
 
-        System.out.println( "REGISTER com.github.straider.osgi.producer.ExampleService" );
+        System.out.println( "REGISTER com.github.straider.osgi.producer.IMailerService" );
 
         // Register our example service implementation in the OSGi service registry
-        bc.registerService( ExampleService.class.getName(), new ExampleServiceImpl(), props );
+        bc.registerService( IMailerService.class.getName(), new MailerService(), props );
     }
 
     /**
      * Called whenever the OSGi framework stops our bundle
      */
-    public void stop( BundleContext bc )
-        throws Exception
-    {
+    public void stop( BundleContext bc ) throws Exception {
         System.out.println( "STOPPING com.github.straider.osgi.producer" );
 
         // no need to unregister our service - the OSGi framework handles it for us
     }
-}
 
+}
