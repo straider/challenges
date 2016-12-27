fa9fc43052d2aae081fab8cf76670000889385df
straider <jctmonteiro@gmail.com>
2016-12-27 13:30:18 +0000

    Reverted JSON request and response handling to Jettison.

--- LearningPath/2016/Java/WebServices/CXF/REST/Standalone/src/main/java/com/github/straider/java/ws/cxf/Server.java
+++ LearningPath/2016/Java/WebServices/CXF/REST/Standalone/src/main/java/com/github/straider/java/ws/cxf/Server.java
@@ -1,9 +1,9 @@
 package com.github.straider.java.ws.cxf;
 
-import com.fasterxml.jackson.jaxrs.base.ProviderBase;
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+import org.apache.cxf.jaxrs.provider.AbstractConfigurableProvider;
+import org.apache.cxf.jaxrs.provider.json.JSONProvider;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -22,12 +22,12 @@ public class Server {
         }
 
         final JAXRSServerFactoryBean serverFactory = new JAXRSServerFactoryBean();
-        serverFactory.setResourceClasses( GreetingService.class );
+        serverFactory.setResourceClasses( GreetingService.class, ChecksumService.class );
         serverFactory.setResourceProvider( GreetingService.class, new SingletonResourceProvider( new GreetingService() ) );
         serverFactory.setAddress( String.format( "http://%s:%d", host, port ) );
 
-        final JacksonJaxbJsonProvider jsonProvider = new JacksonJaxbJsonProvider();
-        final List< ProviderBase >    providers    = new ArrayList< ProviderBase >();
+        final JSONProvider                         jsonProvider = new JSONProvider();
+        final List< AbstractConfigurableProvider > providers    = new ArrayList< AbstractConfigurableProvider >();
         providers.add( jsonProvider );
         serverFactory.setProviders( providers );
         serverFactory.create();
