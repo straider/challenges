b32dde8c6db4a200ce25caa38c93885144a16381
straider <jctmonteiro@gmail.com>
2017-01-30 19:00:24 +0000

    Added Model, Repository and Application classes.

---
+++ LearningPath/2017/SpringBoot/SpringData/MongoDB/Spring Data and MongoDB - Customer Example/src/main/java/com/github/straider/springboot/springdata/mongodb/Customer.java
@@ -0,0 +1,30 @@
+package com.github.straider.springboot.springdata.mongodb;
+
+import org.springframework.data.annotation.Id;
+
+public class Customer {
+
+    @Id
+    public String id;
+
+    public String firstName;
+    public String lastName;
+
+    public Customer() {}
+
+    public Customer( final String firstName, final String lastName ) {
+        this.firstName = firstName;
+        this.lastName  = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                  "Customer[id=%s, firstName='%s', lastName='%s']"
+                , id
+                , firstName
+                , lastName
+        );
+    }
+
+}
