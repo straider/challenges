324b04c17c861cfcf179cfb2066acf9bc9b5fff0
straider <jctmonteiro@gmail.com>
2017-02-14 19:01:57 +0000

    Moved Docket logic out of SpringBoot main application and into SwaggerConfiguration class.

--- LearningPath/2017/SpringBoot/Swagger/GreetingServiceSpringMVC/src/main/java/com/github/straider/springboot/springmvc/swagger/Application.java
+++ LearningPath/2017/SpringBoot/Swagger/GreetingServiceSpringMVC/src/main/java/com/github/straider/springboot/springmvc/swagger/Application.java
@@ -2,13 +2,6 @@ package com.github.straider.springboot.springmvc.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -17,27 +10,6 @@ public class Application {
 
     public static void main( final String[] arguments ) {
         SpringApplication.run( Application.class, arguments );
-    }
-
-    @Bean
-    public Docket swaggerSettings() {
-        return new Docket( DocumentationType.SWAGGER_2 )
-                .groupName( "GreetingService" )
-                .apiInfo( apiInfo() )
-                .select()
-                .apis( RequestHandlerSelectors.any() )
-                .paths( PathSelectors.regex( "/ws/rest/.*" ) )
-                .build()
-                ;
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title( "REST Greeting Service with Swagger" )
-                .description( "Spring MVC REST Greeting Service documented by Swagger." )
-                .contact( "Jose Carlos Monteiro" )
-                .version( "2.0" )
-                .build();
     }
 
 }
---
+++ LearningPath/2017/SpringBoot/Swagger/GreetingServiceSpringMVC/src/main/java/com/github/straider/springboot/springmvc/swagger/SwaggerConfiguration.java
@@ -0,0 +1,38 @@
+package com.github.straider.springboot.springmvc.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket swaggerSettings() {
+        return new Docket( DocumentationType.SWAGGER_2 )
+                .groupName( "GreetingService" )
+                .apiInfo( apiInfo() )
+                .select()
+                .apis( RequestHandlerSelectors.any() )
+                .paths( PathSelectors.regex( "/ws/rest/.*" ) )
+                .build()
+                ;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title( "REST Greeting Service with Swagger" )
+                .description( "Spring MVC REST Greeting Service documented by Swagger." )
+                .contact( "Jose Carlos Monteiro" )
+                .version( "2.0" )
+                .build();
+    }
+
+}
