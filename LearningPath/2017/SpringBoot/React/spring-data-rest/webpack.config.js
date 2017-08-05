39eaf128628af79d418366b347ccb3481aa382cf
straider <jctmonteiro@gmail.com>
2017-08-05 18:08:25 +0100

    Re-arranged folders for more fluid layout.

--- LearningPath/2017/SpringBoot/React/spring-data-rest/webpack.config.js
+++ LearningPath/2017/SpringBoot/React/spring-data-rest/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require( 'path' );
 
 module.exports = {
-    entry: './src/main/javascript/app.js',
+    entry: './src/main/resources/user-interface/app.js',
     devtool: 'sourcemaps',
     cache: true,
     debug: true,
