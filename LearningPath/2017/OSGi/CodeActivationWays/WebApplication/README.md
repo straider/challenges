8be2f3e201be57a4654ae0873ddf01b0f8013c93
straider <jctmonteiro@gmail.com>
2018-02-18 18:45:28 +0000

    Re-arranged LearningPath contents.

--- LearningPath/2017/OSGi/CodeActivationWays/WebApplication/README.md
+++
@@ -1,2 +0,0 @@
-:: OSGi :: Code Activation :: Web Application ::
-================================================
--- LearningPath/2017/SpringBoot/React/spring-data-rest/src/main/java/com/github/straider/springboot/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.straider.springboot;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
